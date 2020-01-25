83f9aba4f4169e09d0593fff4732ac5430dafec4
Jon Cable <jonnyothan@gmail.com>
2020-01-25 09:26:06 -0800

    fix formatting

--- KOS-Scansat/SCANWrapper.cs
+++ KOS-Scansat/SCANWrapper.cs
@@ -10,64 +10,67 @@ namespace kOS.AddOns.kOSSCANsat
 {
 
 
-    internal class SCANWrapper
-    {
-        internal SCANWrapper instance;
+	internal class SCANWrapper
+	{
+		internal SCANWrapper instance;
 
-        internal bool scansatinstalled;
-        internal SCANWrapper()
-        {
-            instance = this;
-            scansatinstalled = Addon.IsModInstalled("scansat");
-        }
-        internal void InitReflection()
-        {
-            //for future use
-        }
+		internal bool scansatinstalled;
 
-        internal int GetSCANtype(string s_type)
-        {
+		internal SCANWrapper()
+		{
+			instance = this;
+			scansatinstalled = Addon.IsModInstalled("scansat");
+		}
+		internal void InitReflection()
+		{
+			//for future use
+		}
+
+		internal int GetSCANtype(string s_type)
+		{
 			if (Enum.TryParse(s_type, true, out SCANtype scanType))
 			{
 				return SCANUtil.GetSCANtype(scanType.ToString());
 			}
 
 			throw new KOSException("{0} is not a valid scan type.  Use GetScanNames for valid types.", s_type);
-        }
+		}
 
-        internal bool GetResourceBiomeLock()
-        {
-            bool resourcelock = true;
-            if (scansatinstalled)
-            {
-                resourcelock = SCANUtil.resourceBiomeLockEnabled();
-            }
-            return resourcelock;
-        }
+		internal bool GetResourceBiomeLock()
+		{
+			bool resourcelock = true;
+			if (scansatinstalled)
+			{
+				resourcelock = SCANUtil.resourceBiomeLockEnabled();
+			}
+			return resourcelock;
+		}
 
-        internal bool IsCovered(double lon, double lat, CelestialBody body, string scan_type) {
-            bool iscovered = false;
-            if (scansatinstalled) {
-                iscovered = SCANUtil.isCovered(lon, lat, body, GetSCANtype(scan_type));
-            }
-            return iscovered;
-        }
+		internal bool IsCovered(double lon, double lat, CelestialBody body, string scan_type)
+		{
+			bool iscovered = false;
+			if (scansatinstalled)
+			{
+				iscovered = SCANUtil.isCovered(lon, lat, body, GetSCANtype(scan_type));
+			}
+			return iscovered;
+		}
 
-        internal double GetCoverage(string scantype, CelestialBody body)
-        {
-            double completed = 0d;
-            if (scansatinstalled)
-            {
-                completed = SCANUtil.GetCoverage(GetSCANtype(scantype), body);
-            }
-            return completed;
-        }
+		internal double GetCoverage(string scantype, CelestialBody body)
+		{
+			double completed = 0d;
+			if (scansatinstalled)
+			{
+				completed = SCANUtil.GetCoverage(GetSCANtype(scantype), body);
+			}
+			return completed;
+		}
 
-        internal List<SCANresourceGlobal> GetLoadedResourceList()
-        {
-            return SCANcontroller.setLoadedResourceList();
-        }
-    
+		internal List<SCANresourceGlobal> GetLoadedResourceList()
+		{
+			return SCANcontroller.setLoadedResourceList();
+		}
 
-    } 
+
+	}
 }
